f04115e92fdeaeba0161c89fe01122c0fd99c4dc
Junko Mikami <junkomikami312nz@gmail.com>
2020-12-27 01:24:29 +0900

    edit indents
    インデント揃えた

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -63,13 +63,13 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:title, :content, :image, :image_cache)
-    end
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :content, :image, :image_cache)
+  end
 end
