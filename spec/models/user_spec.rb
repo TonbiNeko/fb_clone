d19b1a3ff5a0afb9f6f070a48db21935ce1dea57
Junko Mikami <junkomikami312nz@gmail.com>
2021-01-14 17:19:52 +0900

    学びをコメントに残した

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,6 +42,25 @@ RSpec.describe User, type: :model do
   end
 end
 
+# expect(user.errors[:name].first).to include("is too long")
+# はuser.errors[:name].firstに"is too long"が含まれているかどうかを評価している
+# include メソッドは、配列に使うか、文字列に使うかで評価方法が異なる
+
+# 配列⇨完全一致でtrue
+# 文字列⇨完全に一致しなくても、含まれていればtrue
+
+# なのでexpect(user.errors[:name]).to include("is too long")
+# だと、"is too long"は
+# user.errors[:name]の配列に向けてincludeしていて
+# 配列（error文）と完全一致しないのでerrorになってしまった
+# 。firstをつけることにより
+# 配列の中身の１番目を取り出し、文字列のerror文と評価することにより
+# 完全一致でなくなり、一部一致になるのでうまくいった！
+
+# errorが出た時は、まず自分で定義したuser = User.new/create
+# で自分の求めるインスタンスが作られているのかを確認してから
+# なぜexpectとマッチャがうまく行かないのかを考える！
+
 
 
 
